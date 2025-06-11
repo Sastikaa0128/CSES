991f1c229c149637c339ab0d669fcec2c0fa4065
Sastikaa S <mm23b062@smail.iitm.ac.in>
2025-06-11 12:58:55 +0530

    CSES-Missing Number

---
+++ Introductory Problems/Missing Number.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+	long long  n,a,b,c;
+	vector<int> vec;
+	cin >> n;
+	for(int i=0;i<n-1;i++)
+	{
+		cin >> a;
+		vec.push_back(a);
+	}
+	sort(vec.begin(),vec.end());
+	for(int i=0;i<n;i++)
+	{
+ 
+		if(vec[i]!=i+1)
+		{
+			cout << i+1 <<endl;
+			break;
+		}
+	}
+}
